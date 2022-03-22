d9eb8c85efb56120c646ad9c6c5df2f409930876
XingRay <leixing1012@gmail.com>
2022-03-22 17:30:14 +0800

    导出command类

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,6 @@ module com.xingray.javacommand {
     requires com.xingray.commandexecutor;
 
     opens com.xingray.javacommand to com.xingray.commandexecutor;
+
+    exports com.xingray.javacommand;
 }
